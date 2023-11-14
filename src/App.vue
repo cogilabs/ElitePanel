0ce94d4c5343bf7c2e599f8a2fc1fa00d607c485
David Girou <david.girou@0Davgi0.com>
2023-11-14 16:54:56 +0100

    Modified how the menu is made

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="topBar">
-    Cogilabs Elite Panel Configurator
+    Cogilabs ElitePanel Configurator
   </div>
   <div class="menuBar">
     <section ref="items" class="menuBarItems" @click="toggleDrop">
-      <menu-item 
-      button="File"
-      :options="fileItems"
-      :dropped="dropped"
-      :close="closeFile"
-      @hovered="receiveEmit"
-      />
-      <menu-item 
-      button="Profiles"
-      :options="profilesItems"
-      :dropped="dropped"
-      :close="closeProfiles"
-      @hovered="receiveEmit"
-      />
-      <menu-item 
-      button="Settings"
-      :options="settingsItems"
-      :dropped="dropped"
-      :close="closeSettings"
-      @hovered="receiveEmit"
+      <menu-item
+        v-for="(item, index) in menuItems"
+        :key="index"
+        :name="item.name"
+        :options="item.options"
+        :dropped="dropped"
+        :close="item.close"
+        @hovered="receiveEmit"
       />
     </section>
   </div>
@@ -33,54 +21,68 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        dropped: false,
-        fileItems: [
-          { name: "Option 1" },
-          { name: "Option 2" },
-          { name: "Option 3" }
-        ],
-        closeFile: false,
-        profilesItems: [ // TODO: Make interactive
-          { name: "Create profile" },
-          { name: "---" },
-          { name: "Profile 1" },
-          { name: "Profile 2" },
-          { name: "Profile 3" }
-        ],
-        closeProfiles: false,
-        settingsItems: [
-          { name: "Setting 1" },
-          { name: "Setting 2" },
-          { name: "Setting 3" }
-        ],
-        closeSettings: false
-      }
-    },
-    methods: {
+export default {
+  data() {
+    return {
+      dropped: false,
+      menuItems: [
+        {
+          name: "File",
+          options: [
+            { name: "Option 1" },
+            { name: "Option 2" },
+            { name: "Option 3" },
+          ],
+          close: false,
+        },
+        {
+          name: "Profiles",
+          options: [
+            { name: "Create profile" },
+          ],
+          close: false,
+        },
+        {
+          name: "Settings",
+          options: [
+            { name: "Setting 1" },
+            { name: "Setting 2" },
+            { name: "Setting 3" },
+          ],
+          close: false,
+        },
+      ],
+    };
+  },
+  beforeMount() {
+    this.loadProfiles();
+  },
+  methods: {
       receiveEmit(item) {
-        if (item == "File") {
-          this.closeProfiles = !this.closeProfiles
-          this.closeSettings = !this.closeSettings
-        }
-        if (item == "Profiles") {
-          this.closeFile = !this.closeFile
-          this.closeSettings = !this.closeSettings
-        }
-        if (item == "Settings") {
-          this.closeFile = !this.closeFile
-          this.closeProfiles = !this.closeProfiles
-        }
-      }, 
+        this.menuItems.forEach((menuItem) => {
+          if (menuItem.name !== item) {
+            menuItem.close = !menuItem.close;
+          }
+        });
+      },
+      loadProfiles() {
+        this.menuItems.forEach((menuItem) => {
+          if (menuItem.name == "Profiles") {
+            console.log(typeof(menuItem.options))
+            menuItem.options.push({ name: "---" });
+            for (let i = 1; i <= 3; i++) {
+              menuItem.options.push({ name: "Profile " + i });
+            }
+          }
+        });
+      },
       toggleDrop() {
         var _this = this
-        const closeListerner = (e) => {
+        const closeListener = (e) => {
           if ( _this.catchOutsideClick(e, _this.$refs.items ) )
-            window.removeEventListener('click', closeListerner) , _this.dropped = false
+            window.removeEventListener('click', closeListener) , _this.dropped = false
         }
-        window.addEventListener('click', closeListerner)
+        window.addEventListener('click', closeListener)
         this.dropped = !this.dropped
       },
       catchOutsideClick(event, items)  {
